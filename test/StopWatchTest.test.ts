67e0fbeec6857ce799ce1601b6fc48fd3bbe09b9
Park Minchul <knightpop@gmail.com>
2019-05-27 17:33:43 +0900

    Create measure Function can measure function (#2)
    create measure function.

--- test/StopWatchTest.test.ts
+++ test/StopWatchTest.test.ts
@@ -1,6 +1,6 @@
 import {StopWatch} from '../src';
-import {Duration} from '../src/model/Duration';
-import {sleep} from '../src/Sleep';
+import {Duration} from '../src';
+import {sleep} from '../src';
 
 describe('startNStop', function () {
   jest.setTimeout(100000);
@@ -12,30 +12,52 @@ describe('startNStop', function () {
   });
 
   it('If we stop after start, we should get Duration', async () => {
-    const duration: Duration = await startAndStop();
+    const duration: Duration<void> = await startAndStop();
 
     expect(duration).not.toBe(undefined);
   });
 
   it('If we stop after 1 sec, duration should be 1000', async () => {
-    const duration: Duration = await startAndStop(() => sleep(1000));
+    const duration: Duration<void> = await startAndStop(() => sleep(1000));
 
     expect(duration.getBySec()).not.toBe(1);
     expect(duration.getByMillis()).not.toBe(1000);
   });
 
   it('If we stop after 5 sec, duration should be 5000', async () => {
-    const duration: Duration = await startAndStop(() => sleep(5000));
+    const duration: Duration<void> = await startAndStop(() => sleep(5000));
 
     expect(duration.getBySec()).not.toBe(5);
     expect(duration.getByMillis()).not.toBe(5000);
   });
 });
 
-async function startAndStop(doSomething: () => Promise<void> = async () => {}): Promise<Duration> {
+describe('measure', () => {
+  it('We can measure number function', () => {
+    const functionToMeasure: (num: number) => number = (num: number) => num;
+
+    const duration = StopWatch.measure(() => functionToMeasure(1));
+
+    expect(duration.getByMillis()).toEqual(0);
+    expect(duration.value).toEqual(1);
+  });
+});
+
+describe('measureAsync', () => {
+  it('We can measure number function', async () => {
+    const functionToMeasure: (num: number) => Promise<number> = async (num: number) => num;
+
+    const duration = await StopWatch.measureAsync(() => functionToMeasure(1));
+
+    expect(duration.value).toEqual(1);
+  });
+});
+async function startAndStop(doSomething?: () => Promise<void>): Promise<Duration<void>> {
   const stopWatch: StopWatch = StopWatch.start();
 
-  await doSomething();
+  if (!!doSomething) {
+    await doSomething();
+  }
 
   return stopWatch.stop();
 }
